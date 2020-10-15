f55901c09ab55b09b4794ee8f7ee21081352931a
BillCai06 <billhowkens@gmail.com>
2020-10-15 10:34:31 -0400

    hw3_it works now

--- HW3/rtol.cpp
+++ HW3/rtol.cpp
@@ -4,139 +4,160 @@
 #include <unistd.h>
 #endif
 #include <cstdlib>
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 unsigned int microseconds;
 
- FILE *LEDHandle = NULL;
-  char *LEDBrightness0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
-  char *LEDBrightness1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
-  char *LEDBrightness2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
-  char *LEDBrightness3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
+FILE *LEDHandle = NULL;
+char *LEDBrightness0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
+char *LEDBrightness1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
+char *LEDBrightness2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
+char *LEDBrightness3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
 
-void left(int t){
+void left(int t)
+{
 
-    if((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-   usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
- 
-    if((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
+  if ((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
 
-    if((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    if((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
 }
-void right(int t){
+void right(int t)
+{
 
-    if((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-   usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
- 
-    if((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
 
-    if((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    if((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL){
-      fwrite("1", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
- 
-    usleep(t);
- 
-    if((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL){
-      fwrite("0", sizeof(char), 1, LEDHandle);
-      fclose(LEDHandle);
-    }
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  usleep(t);
+
+  if ((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
 }
- 
-int main(int argc, char** argv) {
-    int tii;
-    char LorR;
-    cout << "Type L or R: "; // Type a number and press enter
-    cin >> LorR; // Get user input from the keyboard
-    cout << "Time in us "; 
-    cin >> tii;
- 
-  while(LorR != '\0' && tii != 0 ){
-    if(LorR =='R'){
-        right(tii);
-    }else if(LorR =='L'){
-        left(tii);
-    }else{
-        break;
-    }
 
+int main(int argc, char **argv)
+{
+  int tii;
+  char LorR;
+  cout << "Type L or R: "; // Type a number and press enter
+  cin >> LorR;             // Get user input from the keyboard
+  cout << "Time in us ";
+  cin >> tii;
+
+  while (LorR != '\0' && tii != 0)
+  {
+    if (LorR == 'R')
+    {
+      right(tii);
+    }
+    else if (LorR == 'L')
+    {
+      left(tii);
+    }
+    else
+    {
+      break;
+    }
   }
   return 0;
- 
 }
