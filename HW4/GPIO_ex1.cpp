30a11bdff2c9628e715d69c5e49a39801af02991
BillCai06 <billhowkens@gmail.com>
2020-10-19 09:19:23 -0400

    GPIO

---
+++ HW4/GPIO_ex1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unistd.h> //for usleep
+#include "GPIO.h"
+using namespace exploringBB;
+using namespace std;
+int main()
+{
+    GPIO outGPIO(60), inGPIO(46);
+    // set GPIO directions
+    outGPIO.setDirection(OUTPUT);
+    inGPIO.setDirection(INPUT);
+    int state = 0;
+    while (1)
+    {
+        if (state == 0)
+        {
+            //button pullup stuck until press
+            while (inGPIO.getValue() == 1)
+                ;
+            usleep(20000); //slowdown
+            //stuck until release
+            while (inGPIO.getValue() == 0)
+                ;
+            state = 1;
+            //call to setValue once
+            outGPIO.setValue(HIGH);
+        }
+        else
+        {
+            while (inGPIO.getValue() == 1)
+                ;
+            usleep(20000);
+            while (inGPIO.getValue() == 0)
+                ;
+            outGPIO.setValue(LOW);
+            state = 0;
+        }
+    }
+    return 0;
+}
