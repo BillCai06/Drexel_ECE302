905d15d5cdaf2bbb0c034b555fc233f457efb7b8
BillCai06 <billhowkens@gmail.com>
2020-10-20 15:22:06 -0400

    hw4_2

---
+++ HW4/GPIO_Timer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unistd.h> //for usleep
+#include "GPIO.h"
+#include "timercpp.h"
+
+
+int main()
+{
+    GPIO outGPIO(60), inGPIO(46);
+    // set GPIO directions
+    outGPIO.setDirection(OUTPUT);
+    inGPIO.setDirection(INPUT);
+    int state = 0;
+    while (1)
+    {
+        if (state == 0)
+        {
+
+            //call to setValue once
+            while (state == 0)
+            {
+
+                if (inGPIO.getValue() == 1)
+                {
+                    Timer t = Timer();
+                    state = 1;
+                }
+            }
+        }
+else
+        {
+
+            while (state == 1)
+            {
+               
+                if (inGPIO.getValue() == 1)
+                {
+                     t.stop();
+                     cout << "Time pasted"<< t << endl;   
+                }
+            }
+        }
+    }
+    return 0;
+}
