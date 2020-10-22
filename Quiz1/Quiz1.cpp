f0eb718e9c2d5c1a06f0e7ad47a3f40224b82910
BillCai06 <billhowkens@gmail.com>
2020-10-22 09:15:02 -0400

    quiz1

---
+++ Quiz1/Quiz1.cpp
@@ -0,0 +1,109 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+unsigned int microseconds;
+
+FILE *LEDHandle = NULL;
+char *LEDBrightness0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
+char *LEDBrightness1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
+char *LEDBrightness2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
+char *LEDBrightness3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
+
+void greater()
+{
+  if ((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+}
+void less()
+{
+
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+}
+void equal()
+{
+
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("1", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+}
+void off()
+{
+
+  if ((LEDHandle = fopen(LEDBrightness0, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness1, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+
+  if ((LEDHandle = fopen(LEDBrightness2, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+
+  if ((LEDHandle = fopen(LEDBrightness3, "r+")) != NULL)
+  {
+    fwrite("0", sizeof(char), 1, LEDHandle);
+    fclose(LEDHandle);
+  }
+}
+
+int main(int argc, char **argv)
+{
+  int Int0;
+  int Int1;
+  cout << "First int "; // Type a number and press enter
+  cin >> Int0;             // Get user input from the keyboard
+  cout << "Second int ";
+  cin >> Int1;
+
+  while (Int0 != 0 && Int1 != 0)
+  {
+    if (Int0 > Int1)
+    {
+      greater();
+      usleep(5000000);
+      off();
+    }
+    else if (Int0 < Int1)
+    {
+      less();
+      usleep(5000000);
+      off();
+    }
+    else if (Int0 == Int1)
+    {
+      equal();
+      usleep(5000000);
+      off();
+    }else{
+      count << "check input ";
+      break;
+    }
+  }
+  return 0;
+}
