4efea1c1f47c70d563a4c412d6d7e5d3d4d51781
BillCai06 <billhowkens@gmail.com>
2020-10-29 09:33:58 -0400

    midterm

--- exame1/Untitled-2.cpp
+++ exame1/Untitled-2.cpp
@@ -14,7 +14,7 @@ char *LEDBrightness0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
 char *LEDBrightness1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
 char *LEDBrightness2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
 char *LEDBrightness3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
-char *LEDS[4] = {LEDBrightness0, LEDBrightness1, LEDBrightness2, LEDBrightness3};
+char *LEDS[7] = {LEDBrightness0, LEDBrightness1, LEDBrightness2, LEDBrightness3,LEDBrightness2,LEDBrightness1,LEDBrightness0};
 
 using namespace exploringBB;
 using namespace std;
@@ -27,7 +27,7 @@ int main()
     int state = 0;
     while (1)
     {
-        for (int i = 0; i < 4)
+        for (int i = 0; i < 5)
         {
 
             if ((LEDHandle = fopen(LEDS[i], "r+")) != NULL)
