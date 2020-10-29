9e525496407079cd79841c6216313d902e0e6a96
BillCai06 <billhowkens@gmail.com>
2020-10-29 09:45:31 -0400

    midterm

--- exame1/Untitled-2.cpp
+++ exame1/Untitled-2.cpp
@@ -14,7 +14,7 @@ char *LEDBrightness0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
 char *LEDBrightness1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
 char *LEDBrightness2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
 char *LEDBrightness3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
-char *LEDS[7] = {LEDBrightness0, LEDBrightness1, LEDBrightness2, LEDBrightness3,LEDBrightness2,LEDBrightness1,LEDBrightness0};
+char *LEDS[7] = {LEDBrightness0, LEDBrightness1, LEDBrightness2, LEDBrightness3, LEDBrightness2, LEDBrightness1, LEDBrightness0};
 
 using namespace exploringBB;
 using namespace std;
@@ -22,13 +22,11 @@ int main()
 {
     GPIO inGPIO(46);
     inGPIO.setDirection(INPUT);
-  
+
     while (1)
     {
         for (int i = 0; i < 6;)
         {
-
-
 
             if (inGPIO.getValue() == 1)
             {
@@ -40,10 +38,19 @@ int main()
                     fwrite("1", sizeof(char), 1, LEDHandle);
                     fclose(LEDHandle);
                 }
-                 if ((LEDHandle = fopen(LEDS[i-1], "r+")) != NULL)
+                if ((LEDHandle = fopen(LEDS[i - 1], "r+")) != NULL)
                 {
                     fwrite("0", sizeof(char), 1, LEDHandle);
                     fclose(LEDHandle);
+                }
+                if (i == 6)
+                {
+
+                    if ((LEDHandle = fopen(LEDS[0], "r+")) != NULL)
+                    {
+                        fwrite("0", sizeof(char), 1, LEDHandle);
+                        fclose(LEDHandle);
+                    }
                 }
                 i++;
             }
