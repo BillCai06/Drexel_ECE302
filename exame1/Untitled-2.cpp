7c3330f2a8c3998647e6cbff2c3c81ba32957136
BillCai06 <billhowkens@gmail.com>
2020-10-29 09:24:28 -0400

    midterem

---
+++ exame1/Untitled-2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+#include "GPIO.h"
+
+using namespace std;
+unsigned int microseconds;
+
+FILE *LEDHandle = NULL;
+char *LEDBrightness0 = "/sys/class/leds/beaglebone:green:usr0/brightness";
+char *LEDBrightness1 = "/sys/class/leds/beaglebone:green:usr1/brightness";
+char *LEDBrightness2 = "/sys/class/leds/beaglebone:green:usr2/brightness";
+char *LEDBrightness3 = "/sys/class/leds/beaglebone:green:usr3/brightness";
+char *LEDS[4] = {LEDBrightness0, LEDBrightness1, LEDBrightness2, LEDBrightness3};
+
+using namespace exploringBB;
+using namespace std;
+int main()
+{
+    GPIO inGPIO(46);
+    // set GPIO directions
+    outGPIO.setDirection(OUTPUT);
+    inGPIO.setDirection(INPUT);
+    int state = 0;
+    while (1)
+    {
+        for (int i = 0; i < 4)
+        {
+
+            if ((LEDHandle = fopen(LEDS[i], "r+")) != NULL)
+            {
+                fwrite("1", sizeof(char), 1, LEDHandle);
+                fclose(LEDHandle);
+            }
+
+            if (inGPIO.getValue() == 1)
+            {
+                while (inGPIO.getValue() == 1)
+                    ;
+
+                if ((LEDHandle = fopen(LEDS[i], "r+")) != NULL)
+                {
+                    fwrite("0", sizeof(char), 1, LEDHandle);
+                    fclose(LEDHandle);
+                }
+                 if ((LEDHandle = fopen(LEDS[i+1], "r+")) != NULL)
+                {
+                    fwrite("1", sizeof(char), 1, LEDHandle);
+                    fclose(LEDHandle);
+                }
+                i++;
+            }
+        }
+    }
+    return 0;
+}
