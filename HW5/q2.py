b75662e324a47f55579bb6cb58e6c62e5b57a80a
BillCai06 <billhowkens@gmail.com>
2020-11-05 23:32:21 -0500

    cpp suck, no I sucks

--- HW5/q2.py
+++ HW5/q2.py
@@ -1,18 +1,21 @@
 import Adafruit_BBIO.PWM as PWM
- 
+import Adafruit_BBIO.GPIO as GPIO
+
+GPIO.setup("P8_12", GPIO.IN)
+old_switch_state = 0
 servo_pin = "P8_19"
 duty_min = 3
 duty_max = 14.5
 duty_span = duty_max - duty_min
- 
+flag = 0
 PWM.start(servo_pin, (100-duty_min), 60.0,1)
  
 while True:
-    angle = raw_input("Angle (0 to 180 x to exit):")
-    if angle == 'x':
-        PWM.stop(servo_pin)
-        PWM.cleanup()
-        break
+    new_switch_state = GPIO.input("P8_12")
+    if new_switch_state == 1 and old_switch_state == 0 :
+        flag = flag + 1 
+    angle = flag * 45
+    
     angle_f = float(angle)
     duty = 100 - ((angle_f / 180) * duty_span + duty_min) 
     PWM.set_duty_cycle(servo_pin, duty)
