348b573b6d3fb48d9de24a6973a6d1d72279c659
BillCai06 <billhowkens@gmail.com>
2020-11-05 21:10:25 -0500

    hw5w5

--- HW5/q1.cpp
+++ HW5/q1.cpp
@@ -2,9 +2,15 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <unistd.h> //for usleep
+#include "GPIO.h"
+using namespace exploringBB;
+
 using namespace std;
-#define LDR_PATH
-"/sys/bus/iio/devices/iio:device0/in_voltage" int readAnalog(int number)
+
+#define LDR_PATH "/sys/bus/iio/devices/iio:device0/in_voltage"
+
+int readAnalog(int number)
 {
     stringstream ss;
     ss << LDR_PATH << number << "_raw";
@@ -16,9 +22,23 @@ using namespace std;
 }
 int main(int argc, char *argv[])
 {
+    GPIO outGPIO(60), inGPIO(46);
+    // set GPIO directions
+    outGPIO.setDirection(OUTPUT);
+    inGPIO.setDirection(INPUT);
+    int state = 0;
     cout << "Starting the readLDR program" << endl;
-    int value = readAnalog(0);
-    cout << "The LDR value was " << value << " out of
-        4095. " << endl;
-        return 0;
+    while (1)
+    {
+        int value = readAnalog(0);
+        if (value = > 2048)
+        {
+            outGPIO.setValue(LOW);
+        }
+        else
+        {
+            outGPIO.setValue(HIGH);
+        }
+    }
+    return 0;
 }
