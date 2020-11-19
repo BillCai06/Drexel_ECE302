1f6db46d928bff7bdb575745e75418b8e24898e5
BillCai06 <billhowkens@gmail.com>
2020-11-19 09:46:31 -0500

    quiz2

--- Quiz2/Q1.py
+++ Quiz2/Q1.py
@@ -9,7 +9,7 @@ duty_min = 3
 duty_max = 14.5
 duty_span = duty_max - duty_min
 
-PWM.start(servo_pin, (100-duty_min), 60.0)
+PWM.start(servo_pin, (100-duty_min), 60.0,1)
 angle_f = float(90)
 duty = 100 - ((angle_f / 180) * duty_span + duty_min) 
 duty_zero = 100 - (0 * duty_span + duty_min) 
@@ -19,9 +19,8 @@ print('Reading\t\tVolts')
  
 while True:
     reading = ADC.read(sensor_pin)
-    volts = reading
-    print('%f\t%f' % (reading, volts))
-    if volts >=0.85:
+    print(reading)
+    if reading >=0.85:
         PWM.set_duty_cycle(servo_pin, duty)
     else:
         PWM.set_duty_cycle(servo_pin, duty_zero)
